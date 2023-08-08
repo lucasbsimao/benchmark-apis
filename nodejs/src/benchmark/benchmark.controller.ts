1a65172d95a0ee4b39913f4a781e0c5b5b7c187e
Lucas Borsatto Simão <lucasborsattosimao@hotmail.com>
2023-08-08 18:35:22 -0300

    Ajustando benchmark inicial

--- nodejs/src/benchmark/benchmark.controller.ts
+++ nodejs/src/benchmark/benchmark.controller.ts
@@ -1,14 +1,26 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Logger, Post } from '@nestjs/common';
 import { CreateUserRequestDto } from './createUserRequest.dto';
 import * as crypto from 'crypto';
 import { CreateUserResponseDto } from './createUserResponse.dto';
 
 @Controller('benchmark')
 export class BenchmarkController {
+    private readonly logger = new Logger(BenchmarkController.name);
 
     @Post()
-    createUser(@Body() createUserDto: CreateUserRequestDto) : CreateUserResponseDto {
-        const hashed = crypto.createHash("sha256").update(createUserDto.name).digest("base64");
+    async createUser(@Body() createUserDto: CreateUserRequestDto) : Promise<CreateUserResponseDto> {
+
+        
+
+        return await this.test(createUserDto);
+    }
+
+    async test(createUserDto: CreateUserRequestDto) : Promise<CreateUserResponseDto> {
+        let hashed = crypto.createHash("sha256").update(createUserDto.name).digest("base64");
+        for (let i = 0; i < 100000; i++){
+
+            hashed = crypto.createHash("sha256").update(hashed).digest("base64");
+        }
 
         return new CreateUserResponseDto(hashed);
     }
--- nodejs/src/benchmark/benchmark.module.ts
+++ nodejs/src/benchmark/benchmark.module.ts
@@ -1,6 +1,5 @@
 import { Module } from '@nestjs/common';
 import { BenchmarkController } from './benchmark.controller';
-import { BenchmarkService } from './benchmark.service';
 
 @Module({
   controllers: [BenchmarkController],
