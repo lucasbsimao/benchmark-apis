b5734c8f7c1002db6bc56f58d1de5cfac4d503e4
Lucas Borsatto Simão <lucasborsattosimao@hotmail.com>
2023-09-05 00:31:22 -0300

    Corrigindo stdout do nodejs

--- nodejs/runBenchmark.sh
+++ nodejs/runBenchmark.sh
@@ -2,7 +2,7 @@
 
 npm i
 npm run build
-npm run start:prod 2>&1 &
+npm run start:prod > /dev/null 2>&1 &
 
 while [ -z $EXEC_PID ]; do
     EXEC_PID=$(ps aux | grep "node dist/main" | grep -v "grep" | grep -v "sh -c" | awk '{print $2}')
