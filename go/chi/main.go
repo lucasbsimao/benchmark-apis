d949e3f0a2b02d4b06cbf1ba28e0e4f128539ffd
Lucas Borsatto Simão <lucasborsattosimao@hotmail.com>
2025-04-12 00:51:11 -0300

    Change to GC CPU usage

--- go/chi/main.go
+++ go/chi/main.go
@@ -3,35 +3,34 @@ package main
 import (
     "net/http"
 
-	"os"
 	"strconv"
-	"fmt"
 
-	"crypto/sha256"
+    "math"
 
     "github.com/go-chi/chi"
 )
+
+func compute(n int) int {
+	result := 0
+
+    temp := make([]int, n)
+	for i := 0; i < n; i++ {
+		temp[i] = int(math.Sqrt(float64(i*i + i)))
+		result += temp[i]
+	}
+	
+    return result
+}
 
 func get(w http.ResponseWriter, r *http.Request) {
 
 	nStr := r.URL.Query().Get("n")
 
-    n, err := strconv.Atoi(nStr)
+    n, _ := strconv.Atoi(nStr)
 
-	data, err := os.ReadFile("/tmp/txt")
-    if err != nil {
-        fmt.Println(err)
-        return
-    }
+	result := compute(n)
 
-	
-    content := string(data)
-    bytes := []byte(content)
-
-	for i := 0; i < n; i++ {
-		sha256.Sum256(bytes)
-	}
-
+    w.Header().Set("X-Benchmark-Result", strconv.Itoa(result))
 	
     w.Write([]byte("OK"))
 }
